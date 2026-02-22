2f2910d6a527a4c33ad5abf085da84ec7ee710ba
Mansolo-21 <solomonjohari@gmail.com>
2026-02-22 21:24:50 +0300

    commit

--- college/col/urls.py
+++ college/col/urls.py
@@ -1,14 +1,21 @@
 from django.urls import path
 from  . import views
 
-
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
-    path('students-life/', views.students_life, name='students_life'),
-    path('news/', views.news, name='news'),
-    path('events/', views.events, name='events'),
-    path('contact/', views.contact, name='contact'),
     path('admissions/', views.admissions, name='admissions'),
     path('academics/', views.academics, name='academics'),
+    path('faculty/', views.faculty, name='faculty'),
+    path('campus/', views.campus, name='campus'),
+    path('students_life/', views.students_life, name='students_life'),
+    path('news/', views.news, name='news'),
+    path('events/', views.events, name='events'),
+    path('alumni/', views.alumni, name='alumni'),
+    path('error/', views.error, name='error'),
+    path('newsd/', views.newsd, name='newsd'),
+    path('privacy/', views.privacy, name='privacy'),
+    path('terms/', views.terms, name='terms'),
+    path('event-details/', views.eventd, name='eventd'),
+    path('contact/', views.contact, name='contact'),
 ]
--- college/col/views.py
+++ college/col/views.py
@@ -4,24 +4,19 @@ from .models import Contact
 
 
 def index(request):
-    return render(request, 'index.html')
-
+    return render(request, 'col/index.html')
 
 def about(request):
-    return render(request, 'about.html')
-
+    return render(request, 'col/about.html')
 
 def students_life(request):
-    return render(request, 'students_life.html')
-
+    return render(request, 'col/students_life.html')
 
 def news(request):
-    return render(request, 'news.html')
-
+    return render(request, 'col/news.html')
 
 def events(request):
-    return render(request, 'events.html')
-
+    return render(request, 'col/events.html')
 
 def contact(request):
     if request.method == 'POST':
@@ -39,11 +34,48 @@ def contact(request):
         messages.success(request, "Your message has been sent successfully!")
         return redirect('contact')
 
-    return render(request, 'contact.html')
+    return render(request, 'col/contact.html')
 
 def academics(request):
-    return render(request, 'academics.html')
+    return render(request, 'col/academics.html')
 
 def admissions(request):
-    return render(request, 'admissions.html')
+    return render(request, 'col/admissions.html')
+
+def faculty(request):
+    return render(request, 'col/faculty-staff.html')
+
+def campus(request):
+    return render(request, 'col/campus-facilities.html')
+
+def alumni(request):
+    return render(request, 'col/alumni.html')
+
+def eventd(request):
+    return render(request, 'col/event-details.html')
+
+def privacy(request):
+    return render(request, 'col/privacy.html')
+
+def terms(request):
+    return render(request, 'col/terms-of-service.html')
+
+def newsd(request):
+    return render(request, 'col/news-details.html')
+
+def error(request):
+    return render(request, 'col/404.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
 
