ff8fec7e00c39f23097df67600e2c4d280829399
natalie-esuha <natalie.esuha@strathmore.edu>
2026-02-24 16:23:03 +0300

    commit

--- college/col/models.py
+++ college/col/models.py
@@ -14,7 +14,7 @@ class Student(models.Model):
     first_name=models.CharField(max_length=20)
     age=models.IntegerField
     course=models.CharField(max_length=30)
-    telephone=models.IntegerField
+    phone=models.IntegerField
     
     def __str__(self):
         return f'Student name:{self.first_name} Course:{self.course}'
