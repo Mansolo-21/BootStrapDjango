c39aaf396ad9dd34ce5bd084f0f1c2a97d5b29ba
natalie-esuha <natalie.esuha@strathmore.edu>
2026-02-24 14:27:19 +0300

    html

--- college/col/models.py
+++ college/col/models.py
@@ -3,12 +3,12 @@ from django.db import models
 class Contact(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
-    subject =models.CharField(max_length=30)
+    subject = models.CharField(max_length=30, default="No subject")
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'Name :{self.name} Message:{self.message}'
+        return f'Name: {self.name} | Subject: {self.subject}'
     
 class Student(models.Model):
     first_name=models.CharField(max_length=20)
